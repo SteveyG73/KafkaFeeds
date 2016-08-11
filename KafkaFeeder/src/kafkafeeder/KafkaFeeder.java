b3438b1bfda8dc284bf32aaca77918083701ce34
Steve <Steve@DEEP-THOUGHT3>
2016-08-12 00:01:27 +0100

    Added file scanner

--- KafkaFeeder/src/kafkafeeder/KafkaFeeder.java
+++ KafkaFeeder/src/kafkafeeder/KafkaFeeder.java
@@ -5,6 +5,11 @@
  */
 package kafkafeeder;
 import org.apache.logging.log4j.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.json.*;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.*;
 /**
  *
  * @author Steve
@@ -15,18 +20,50 @@ public class KafkaFeeder {
      * @param args the command line arguments
      */
     
-    private static final Logger LOG = LogManager.getLogger();
+    private static final Logger LOG = LogManager.getLogger("kafkafeeder");
     
     public static void main(String[] args) {
         // TODO code application logic here
+
         String kafkaTopic;
-        
+        int i = 0;
+        Properties props = new Properties();
+               
         if (args.length!=1) {
             System.out.println("Incorrect number of parameters supplied...");
+            LOG.error("Incorrect number of parameters supplied...");
             System.exit(1);
         }
         kafkaTopic = args[0];        
         LOG.info("Setting Kafka topic to "+kafkaTopic);
+        props.put("bootstrap.servers", "192.168.1.92:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        
+        LOG.info("Connecting to "+kafkaTopic);
+        Producer<String, String> kafka = new KafkaProducer<>(props);
+        LOG.info("Connection successful");
+        
+        LOG.info("Building file list...");
+        MP3List mp3s = new MP3List("D:\\Music");
+        
+        LOG.info("Sending files...");
+        while (i < mp3s.getMP3List().size()) {
+
+          String payload = mp3s.getMP3List().get(i).toString();
+          i++;
+          kafka.send(new ProducerRecord<String, String>(kafkaTopic, Integer.toString(i), payload ));
+        }
+        
+        LOG.info("Closing Connectiion to "+kafkaTopic);        
+        kafka.close();
+        LOG.info("Connection successfully dropped");
+        
     }
     
 }
---
+++ KafkaFeeder/src/kafkafeeder/MP3List.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kafkafeeder;
+
+import org.apache.logging.log4j.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+import com.mpatric.mp3agic.*;
+import javax.json.*;
+/**
+ *
+ * @author Steve
+ */
+public class MP3List {
+
+    
+    private static final Logger LOG = LogManager.getLogger("kafkafeeder");
+    private List<JsonObject> mp3Json;
+    /**
+     * @param fpath
+     */
+    public MP3List(String fpath) {
+        
+        mp3Json = new ArrayList(); 
+        
+        try (Stream<Path> paths = Files.walk(Paths.get(fpath))){
+            paths.forEach((f) -> {
+               String fname = f.toString();
+               if (fname.endsWith("mp3") ) {
+                  LOG.debug("Adding "+fname);
+                  try {
+                       Mp3File mp3 = new Mp3File(fname);
+                       if (mp3.hasId3v2Tag()) {
+                           ID3v2 tags = mp3.getId3v2Tag();
+                           JsonObject json = Json.createObjectBuilder()
+                                .add("filename", fname)
+                                .add("title", tags.getTitle())
+                                .add("artist", tags.getArtist())
+                                .add("album", tags.getAlbum())
+                                .add("genre", tags.getGenreDescription())
+                                .add("track", tags.getTrack())
+                                .add("year", tags.getYear())
+                                .build();
+                           mp3Json.add(json);
+                           LOG.debug(json.toString());
+                       }
+                  }
+                  catch (Exception e) {
+                      LOG.debug(e.getMessage());
+                  }
+               }
+            });
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<JsonObject> getMP3List() {
+        return mp3Json;
+    }
+}
